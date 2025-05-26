85b73c10220e574b6e442fe0216d64cdfd39072a
elkurto <kurt.buehner@gmail.com>
2025-05-26 17:50:01 -0400

    added tests

--- ab-as-import-fn/tests/index.js
+++ ab-as-import-fn/tests/index.js
@@ -1,5 +1,10 @@
 import assert from "assert";
-import { add } from "../build/debug.js";
-assert.strictEqual(add(1, 2), 3);
+import { add,subtract,hello  } from "../build/debug.js";
 
+assert.strictEqual(add(1, 2), 3);
+console.log('add passed');
+assert.strictEqual(subtract(1, 2), -1);
+console.log('subtract passed');
+assert.strictEqual(hello('foo'), 'hello, foo!');
+console.log('hello passed');
 console.log("ok");
